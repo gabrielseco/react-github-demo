80a98b9fe041f2ede9213f85ee0cbd21eb739240
gabriel <ggarciaseco@gmail.com>
2018-03-24 18:37:52 +0100

    feat: Add a spinner

--- src/components/Spinner/Spinner.js
+++ src/components/Spinner/Spinner.js
@@ -1,11 +1,7 @@
 // @flow
 import React from 'react';
-/* import styles from './Spinner.scss';
- */
-const Spinner = () => (
-  <div>
-    <p>Loading data from server</p>
-  </div>
-);
+import styles from './Spinner.scss';
+
+const Spinner = () => <div className={styles.spinner} />;
 
 export default Spinner;
